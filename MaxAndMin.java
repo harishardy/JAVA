a9134088babed3a940628a1945876308f6ef3614
Harish Hardy <48213328+harishardy@users.noreply.github.com>
2019-03-11 11:47:01 +0530

    Create MaxAndMin.java

---
+++ MaxAndMin.java
@@ -0,0 +1,83 @@
+import java.util.Scanner;
+
+public class MaxAndMin 
+{
+	public static long maximum(long a,long b)
+	{
+		if(a>b)
+		{
+			return a;
+		}
+		else if(b>a)
+		{
+			return b;
+		}
+		else
+		{
+			return a;
+		}
+	}
+	
+	
+	public static long minimum(long a,long b)
+	{
+		if(a<b)
+		{
+			return a;
+		}
+		else if(b<a)
+		{
+			return b;
+		}
+		else
+		{
+			return a;
+		}
+	}
+	
+	public static void main(String[] args) 
+	{
+		long t=0,y=999999999;
+		
+		Scanner scan=new Scanner(System.in);
+		
+		System.out.println("Enter the number of elements you want to compare: ");
+		
+		int length=scan.nextInt();
+		
+		System.out.println();
+		
+		long[] elements=new long[100];
+		
+		for(int i=0;i<length;i++)
+		{
+			System.out.println("Enter the "+(i+1)+" position number: ");
+			
+			elements[i]= scan.nextInt();
+		}
+		
+		scan.close();
+		
+		System.out.println();
+		
+		for(int i=0;i<length;i++)
+		{
+			t=maximum(t,elements[i]);
+			
+		}
+		
+		System.out.println("The Biggest value of the entered elements is:\n\n"+"   "+t);
+		
+		System.out.println();
+		
+		for(int i=0;i<length;i++)
+		{
+			y=minimum(y,elements[i]);
+		}
+		
+		System.out.println("The Smallest value of the entered elements is:\n\n"+"   "+y);	
+		
+	}
+
+	
+}
